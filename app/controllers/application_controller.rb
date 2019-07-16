7a9c59089d482f7a68dc933c6e22e601450a69ec
github username <github email address>
2019-07-16 14:54:02 -0500

    updated lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,46 @@ class ApplicationController < Sinatra::Base
         set :method_override, true
       end
     
-      get '/' do
-        erb :welcome
-      end
+      # get '/puppies' do
+      #   # binding.pry
+      #   @puppys = Puppy.all
+      #   erb :welcome
+      # end
+
+      # get '/puppies/new' do
+      #   erb :new
+      # end
+
+      # post '/puppies' do
+      #   Puppy.create(params)
+      #   redirect '/puppies'
+      # end
+
+      # get '/puppies/:id' do
+      #   @puppy = Puppy.find(params[:id])
+      #   erb :show
+      # end
+
+      # get '/puppies/:id/edit' do
+      #   @puppy = Puppy.find(params[:id])
+      #   erb :edit
+      # end
+
+      # patch '/puppies/:id' do
+      #   # binding.pry
+      #   puppy = Puppy.find(params[:id])
+      #   puppy.update(name: params[:name], breed: params[:breed])
+      #   redirect "/puppies/#{puppy.id}"
+        
+      # end
+
+      # delete '/puppies/:id' do
+      #   # binding.pry
+      #   puppy = Puppy.find(params[:id])
+      #   puppy.destroy 
+      #   redirect '/puppies'
+      # end
+      
 
    
 end
